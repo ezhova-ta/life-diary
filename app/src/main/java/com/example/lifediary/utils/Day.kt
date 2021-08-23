a5a58e0d38164bf18de36db003dd9e7c96a677ab
ezhova-ta <ezhova.ta.ta@yandex.ru>
2021-08-23 23:06:42 +0300

    Added documentation

--- app/src/main/java/com/example/lifediary/utils/Day.kt
+++ app/src/main/java/com/example/lifediary/utils/Day.kt
@@ -7,7 +7,16 @@ import kotlinx.parcelize.Parcelize
 // TODO Javadoc
 @Parcelize
 data class Day(
+	/**
+	 * Int value for day of month, from 1 to 31
+	 */
 	val dayNumber: Int,
+	/**
+	 * Int value for month of year, from 1 to 12
+	 */
 	val monthNumber: Int,
+	/**
+	 * Int value for year
+	 */
 	val year: Int
 ) : Parcelable
