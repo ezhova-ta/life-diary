713e5817336253399d485b95ffc2bbd542ba6a3b
ezhova-ta <ezhova.ta.ta@yandex.ru>
2022-03-02 15:55:58 +0300

    Fixed scheduling notifications

--- app/src/main/java/com/example/lifediary/NotificationScheduler.kt
+++ app/src/main/java/com/example/lifediary/NotificationScheduler.kt
@@ -16,7 +16,7 @@ class NotificationScheduler @Inject constructor(private val context: Context) {
 	fun scheduleNotification(toDoListItem: ToDoListItem, timeInMillis: Long) {
 		val alarmManager = getAlarmManager()
 		val intent = createNotificationIntent(toDoListItem) ?: return
-		alarmManager.set(
+		alarmManager.setExact(
 			AlarmManager.RTC_WAKEUP,
 			timeInMillis,
 			intent
