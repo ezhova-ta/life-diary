c567e6bc45c00afbc354c4f96faeed984e1e29f5
ezhova-ta <ezhova.ta.ta@yandex.ru>
2022-06-09 21:26:40 +0300

    Refactored javadoc

--- app/src/main/java/com/example/lifediary/utils/DateUtil.kt
+++ app/src/main/java/com/example/lifediary/utils/DateUtil.kt
@@ -145,9 +145,9 @@ fun Calendar.plusDays(amountOfDays: Int): Calendar {
  * Calculates the amount of time between two dates.
  * The result will be negative if the end is before the start
  *
- * @param date1 Calendar object
- * @param date2 Other Calendar object
- * @return The amount of days between date1 (inclusive) and date2 (exclusive).
+ * @param date1 [Calendar][java.util.Calendar] object
+ * @param date2 other [Calendar][java.util.Calendar] object
+ * @return the amount of days between date1 (inclusive) and date2 (exclusive).
  * Positive if date2 is later than date1, negative if earlier
  */
 fun getDaysBetween(date1: Calendar, date2: Calendar): Long {
